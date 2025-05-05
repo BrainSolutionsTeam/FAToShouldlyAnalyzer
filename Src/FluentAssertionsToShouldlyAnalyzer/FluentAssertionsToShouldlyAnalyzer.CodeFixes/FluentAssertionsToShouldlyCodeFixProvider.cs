9c516c25000997e9aa58ffd51b0bdbb234bc260b
Toky <72009671+herostoky@users.noreply.github.com>
2025-05-05 16:17:37 +0300

    wip wip

--- Src/FluentAssertionsToShouldlyAnalyzer/FluentAssertionsToShouldlyAnalyzer.CodeFixes/FluentAssertionsToShouldlyCodeFixProvider.cs
+++ Src/FluentAssertionsToShouldlyAnalyzer/FluentAssertionsToShouldlyAnalyzer.CodeFixes/FluentAssertionsToShouldlyCodeFixProvider.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Composition;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
@@ -26,66 +27,100 @@ namespace FluentAssertionsToShouldlyAnalyzer
             var semanticModel = await context.Document.GetSemanticModelAsync(context.CancellationToken).ConfigureAwait(false);
 
             var diagnostic = context.Diagnostics.First();
-
-            //Register code action that will invoke the fix.
-            context.RegisterCodeFix(
-                CodeAction.Create(
-                    title: CodeFixResources.CodeFixTitle,
-                    createChangedSolution: c => ChangeShouldMethodCall(
-                        root,
-                        semanticModel,
-                        context.Document,
-                        diagnostic),
-                    equivalenceKey: nameof(CodeFixResources.CodeFixTitle)),
-                diagnostic);
-        }
-
-        private static Task<Solution> ChangeShouldMethodCall(
-            SyntaxNode root,
-            SemanticModel semanticModel,
-            Document document,
-            Diagnostic diagnostic)
-        {
             var diagnosticSpan = diagnostic.Location.SourceSpan;
             var invocationNode = root.FindNode(diagnosticSpan);
 
+            var isCodeFixAvailable = IsCodeFixAvailable(
+                invocationNode,
+                semanticModel,
+                out var memberAccessExpression,
+                out var shouldlyMethod,
+                out var nextInvocation);
+
+            if (isCodeFixAvailable)
+            {
+                //Register code action that will invoke the fix.
+                context.RegisterCodeFix(
+                    CodeAction.Create(
+                        title: CodeFixResources.CodeFixTitle,
+                        createChangedSolution: c => ChangeShouldMethodCall(
+                            root,
+                            context.Document,
+                            memberAccessExpression,
+                            shouldlyMethod,
+                            nextInvocation,
+                            c),
+                        equivalenceKey: nameof(CodeFixResources.CodeFixTitle)),
+                    diagnostic);
+
+            }
+        }
+
+        private static bool IsCodeFixAvailable(
+            SyntaxNode invocationNode,
+            SemanticModel semanticModel,
+            out MemberAccessExpressionSyntax memberAccessExpression,
+            out string shouldlyMethod,
+            out InvocationExpressionSyntax nextInvocation)
+        {
+
             if ((invocationNode is IdentifierNameSyntax invocation                                              // invocation = Should
                   && invocation.Parent is MemberAccessExpressionSyntax memberAccess                             // memberAccess = person.Name.Should
-                  && memberAccess.Expression is MemberAccessExpressionSyntax memberAccessExpression             // memberAccessExpression = person.Name
-                  && memberAccessExpression.Expression is IdentifierNameSyntax identifierName                   // identifierName = person
+                  && memberAccess.Expression is MemberAccessExpressionSyntax lMemberAccessExpression            // memberAccessExpression = person.Name
                 ) is false)
             {
-                return Task.FromResult(document.Project.Solution);
+                memberAccessExpression = null;
+                shouldlyMethod = null;
+                nextInvocation = null;
+                return false;
             }
+            memberAccessExpression = lMemberAccessExpression;
 
             var symbolInfo = semanticModel.GetSymbolInfo(memberAccess);
 
             if (FluentAssertionsToShouldlyAnalyzer.IsCallingShouldFromFa(memberAccess, symbolInfo) is false)
             {
-                return Task.FromResult(document.Project.Solution);
+                shouldlyMethod = null;
+                nextInvocation = null;
+                return false;
             }
 
             // Check the next method in the chain
             if ((memberAccess.Parent is InvocationExpressionSyntax shouldMethodInvocation                       // shouldMethodInvocation = person.Name.Should()
                  && shouldMethodInvocation.Parent is MemberAccessExpressionSyntax nextMemberAccess              // nextMemberAccess = person.Name.Should().Be, .NotBe, ...
-                 && nextMemberAccess.Parent is InvocationExpressionSyntax nextInvocation                        // nextInvocation = person.Name.Should().Be("xxx")
+                 && nextMemberAccess.Parent is InvocationExpressionSyntax lNextInvocation                       // nextInvocation = person.Name.Should().Be("xxx")
                  && semanticModel.GetSymbolInfo(nextMemberAccess).Symbol is IMethodSymbol nextMethodSymbol      // nextMethodSymbol = MethodSymbol of `Be`, `NotBe`, etc.
                  ) is false)
             {
-                return Task.FromResult(document.Project.Solution);
+                shouldlyMethod = null;
+                nextInvocation = null;
+                return false;
             }
+            nextInvocation = lNextInvocation;
 
             // TODO: Handle specific case before basic cases, like Should Throw ??
 
             var faReplacement = GetFaReplacement();
 
-            if (faReplacement.TryGetValue(nextMethodSymbol.Name, out var shouldlyMethod) is false)
+            if (faReplacement.TryGetValue(nextMethodSymbol.Name, out var lShouldlyMethod) is false)
             {
-                return Task.FromResult(document.Project.Solution);
+                shouldlyMethod = null;
+                return false;
             }
+            shouldlyMethod = lShouldlyMethod;
 
-            // TODO: OKAY here
+            return true;
+        }
 
+        private static Task<Solution> ChangeShouldMethodCall(
+            SyntaxNode root,
+            Document document,
+            MemberAccessExpressionSyntax memberAccessExpression,
+            string shouldlyMethod,
+            InvocationExpressionSyntax nextInvocation,
+            // ReSharper disable once UnusedParameter.Local
+            CancellationToken cancellationToken = new CancellationToken())
+        {
             // Go for replacement:
             var newMemberAccess = SyntaxFactory.MemberAccessExpression(
                 SyntaxKind.SimpleMemberAccessExpression,
